45f59b72fea511d81e3af998081dae653017551f
Gaudereto <guilherme.gaudereto@engenharia.ufjf.br>
2018-05-09 16:29:41 -0300

    Third version

--- app/src/main/java/com/green/greensolar/MainActivity.java
+++ app/src/main/java/com/green/greensolar/MainActivity.java
@@ -2,36 +2,63 @@ package com.green.greensolar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Variaveis constantes:
+    private List<CityData> CITIES_DATA = new ArrayList<CityData>();
 
     // Referências da interface:
     private AutoCompleteTextView mCityTextView;
     private EditText mConsumeTextView;
     private Button mEstimateButton;
+    private String mCitiesName[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mCityTextView = (AutoCompleteTextView)  findViewById(R.id.city_edit_text);
         mConsumeTextView = (EditText) findViewById(R.id.consumo_edit_text);
         mEstimateButton = (Button) findViewById(R.id.estimate_button);
 
+        // Inicia a lista com cidades e respectivas irradiações:
+
+        // Vetor com os nomes das cidades:
+        for (int i = 0; i < CITIES_DATA.size(); i++) {
+            mCitiesName[i] = CITIES_DATA.get(i).getCity();
+        }
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, CITIES);
+                android.R.layout.simple_dropdown_item_1line, mCitiesName);
         mCityTextView.setAdapter(adapter);
 
+
+        mEstimateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    void initCityData() {
+        CITIES_DATA.add(new CityData("Juiz de Fora",4.71));
     }
 
 
-    private static final String[] CITIES = new String[] {
-            "Juiz de Fora","Belo Horizonte","Rio de Janeiro","São Paulo",
-            "Curitiba","Porto Alegre","Vitória","Salvador"
-    };
+
+
+
 }
